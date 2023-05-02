0669d64bb43797272cfd15bc737de6b56f0cb69d
akash <akash.verma2580@gmail.com>
2023-05-03 01:05:27 +0530

    "update the code"

--- advice-generator-app-main/src/Constant.js
+++ advice-generator-app-main/src/Constant.js
@@ -16,7 +16,7 @@ export const Dice = () => (
 export const Desktop_Divider = () => (
   <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg">
     <g fill="none" fillRule="evenodd">
-      <path fill="hsl(193, 38%, 86%)" d="M0 8h196v1H0zM248 8h196v1H248z" />
+      <path fill="hsl(217, 19%, 38%)" d="M0 8h196v1H0zM248 8h196v1H248z" />
       <g transform="translate(212)" fill="#CEE3E9">
         <rect width="6" height="16" rx="3" />
         <rect x="14" width="6" height="16" rx="3" />
@@ -28,7 +28,7 @@ export const Desktop_Divider = () => (
 export const Mobile_Divider = () => (
   <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
     <g fill="none" fillRule="evenodd">
-      <path fill="hsl(193, 38%, 86%)" d="M0 8h122v1H0zM173 8h122v1H173z" />
+      <path fill="hsl(217, 19%, 38%)" d="M0 8h122v1H0zM173 8h122v1H173z" />
       <g transform="translate(138)" fill="#CEE3E9">
         <rect width="6" height="16" rx="3" />
         <rect x="14" width="6" height="16" rx="3" />
