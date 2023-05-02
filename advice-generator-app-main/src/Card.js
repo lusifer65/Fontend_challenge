3720161502d2098f2f0fbb340a7d36cb750271dc
Akash verma <42277681+lusifer65@users.noreply.github.com>
2023-05-02 15:29:35 +0530

    update Card.js

--- advice-generator-app-main/src/Card.js
+++ advice-generator-app-main/src/Card.js
@@ -1,25 +1,42 @@
 import React, { useEffect, useState } from "react";
-import { Dice } from "./Constant";
+import { Desktop_Divider, Dice, Mobile_Divider } from "./Constant";
 const Card = () => {
   const [advice, setAdvice] = useState("");
+  const [size, setSize] = useState(screen.width);
 
   useEffect(() => {
-    const callAPI = async () => {
-      const data = await fetch("https://api.adviceslip.com/advice").then(
-        (res) => res.json()
-      );
-      setAdvice(data.slip);
-    };
     callAPI();
+    window.addEventListener("resize", (event) => {
+      setSize(screen.width);
+    });
   }, []);
+
+  const callAPI = async () => {
+    const data = await fetch("https://api.adviceslip.com/advice").then((res) =>
+      res.json()
+    );
+    setAdvice(data.slip);
+  };
+
   if (advice == "") {
     return <React.Fragment></React.Fragment>;
   }
   return (
     <React.Fragment>
       <p>ADVICE #{advice.id}</p>
-      <h1>{advice.advice}</h1>
-      <div>
+      <div className="advice">
+        <q>{advice.advice}</q>
+      </div>
+
+      <div className="divider">
+        {size > 375 ? <Desktop_Divider /> : <Mobile_Divider />}
+      </div>
+      <div
+        className="dice"
+        onClick={() => {
+          callAPI();
+        }}
+      >
         <Dice />
       </div>
     </React.Fragment>
